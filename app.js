49b779d803927e983b8ef1824a2e8b568a334596
Joaquin Sarmiento <58922125+JoaStacker@users.noreply.github.com>
2021-03-11 23:07:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 
 document.addEventListener('click', (e)=>{
     if(e.target.className === 'contratar-btn'){
-        window.location.replace('http://127.0.0.1:5500/SMContenedores/producto.html')
+        window.location.replace('./producto.html')
         console.log('pagina cargada')
-        const $form = document.querySelector('form')
-        $form.querySelector('h4').textContent = 'PRODUCTO'
     }
-})
-
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    const $productBTNS = document.querySelectorAll('.contratar-btn') 
-    
 })
